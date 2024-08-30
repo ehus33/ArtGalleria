2d019921307b3003bc262b3bd8d994d3001e4e44
Ethan Hu <ethanh8093@gmail.com>
2024-08-30 09:09:06 -0700

    Create NFT.js

---
+++ backend/models/NFT.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const nftSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  imageUrl: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const NFT = mongoose.model('NFT', nftSchema);
+module.exports = NFT;
