4533751d12403a264bc35916daf01c26b203c48f
Ethan Hu <ethanh8093@gmail.com>
2024-08-30 09:22:30 -0700

    Delete frontend/artgalleria-frontend/smart-contracts directory

--- frontend/artgalleria-frontend/smart-contracts/migrations/1_deploy_contracts.js
+++
@@ -1,6 +0,0 @@
-const ArtGalleriaNFT = artifacts.require("ArtGalleriaNFT");
-
-module.exports = function(deployer) {
-  const baseURI = "https://your-metadata-api.com/metadata/";
-  deployer.deploy(ArtGalleriaNFT, baseURI);
-};
